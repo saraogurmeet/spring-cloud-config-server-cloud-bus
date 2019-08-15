f31e40acfdf3a1de941b114567d84fd340d68548
gursingh25 <gursingh25@WKWCA9441773.mshome.net>
2019-08-14 21:58:59 -0600

    Working changes

--- src/main/java/com/example/demo/SpringCloudTestApplication.java
+++ src/main/java/com/example/demo/SpringCloudTestApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class SpringCloudTestApplication {
 
 	public static void main(String[] args) {
